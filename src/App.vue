5959007852fa6c0bd11ced0f91b3b2ce03f8590d
trizhaaaaa <trizhamaglangit@gmail.com>
2022-05-01 09:01:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="material-kit">
+    <div :class="{ 'nav-open': NavbarStore.showNavbar }">
+      <router-view name="header" />
+      <div>
+        <esc-studDash/>
+        <!-- <router-view /> -->
+      </div>
+      <router-view name="footer" />
+    </div>
+  </div>
+</template>
+
+<script>
+import studDash from "./escord-components/Student/studDashboard.vue";
+
+export default {
+  components:{
+    "esc-studDash": studDash
+  }  
+}
+</script>
