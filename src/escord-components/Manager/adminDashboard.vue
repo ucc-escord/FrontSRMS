d9a087c241684db629b4fc7bd69fd5db23932b26
trizhaaaaa <trizhamaglangit@gmail.com>
2022-05-26 11:19:16 +0800

    admindash shorter version

--- src/escord-components/Manager/adminDashboard.vue
+++ src/escord-components/Manager/adminDashboard.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="wrapper">
+  <div class="wrapper">
     <parallax
       class="section page-header header-filter"
       :style="headerStyle"
@@ -32,108 +32,35 @@
                 <nav-tabs-card no-label tabs-plain>
                 <template slot="content">
                     <md-tabs
-                    md-sync-route
                     class="md-darkgrey" md-alignment="centered">
 
-                        <md-tab id="tab-overview" md-label="Overview"
-                        to="/overview" exact>
-                        
+                        <!-- OVERVIEW -->
+                        <md-tab id="tab-overview" md-label="Overview">
+                            <admin-overview/>
                         </md-tab>
 
+                        <!-- MANAGE ACCOUNT -->
                         <md-tab id="tab-manage-account" md-label="Manage Account">
-                        <p>
-                            I will be the leader of a company that ends up being worth
-                            billions of dollars, because I got the answers. I understand
-                            culture. I am the nucleus. I think that’s a responsibility
-                            that I have, to push possibilities, to show people, this is
-                            the level that things could be at. I think that’s a
-                            responsibility that I have, to push possibilities, to show
-                            people, this is the level that things could be at.
-                        </p>
+                            <admin-manage-account/>
                         </md-tab>
 
+                        <!-- AUDIT TRAIL -->
                         <md-tab id="tab-audit-trail" md-label="Audit Trail">
-                        <p>
-                            I think that’s a responsibility that I have, to push
-                            possibilities, to show people, this is the level that things
-                            could be at. I will be the leader of a company that ends up
-                            being worth billions of dollars, because I got the answers.
-                            I understand culture. I am the nucleus. I think that’s a
-                            responsibility that I have, to push possibilities, to show
-                            people, this is the level that things could be at.
-                        </p>
+                            <admin-audit-trail/>
                         </md-tab>
+                        
                     </md-tabs>
                 </template>
             </nav-tabs-card>
-            
-                <router-view/>
-
-                <!-- <div class="md-layout-item md-size-100">
-
-                    <md-tabs
-                    md-sync-route
-                    class="__tabs">
-
-                        <md-tab
-                        id="tab-overview"
-                        md-label="Overview"
-                        to = ""
-                        exact>hfghghf</md-tab>
-
-                        <md-tab 
-                        id="tab-manage-account"
-                        md-label="Manage Account"
-                        to = "">ghf</md-tab>
-
-                    </md-tabs>
-                </div> -->
-
-
-                <!-- <tabs
-                    :tab-name="['Overview', 'Manage Accounts', 'Audit Trail']"
-                    :tab-icon="['dashboard', 'schedule', 'list']"
-                    plain
-                    nav-pills-icons
-                    color-button="success">
-
-                     here you can add your content for tab-content
-
-                    <template slot="tab-pane-1">
-                        
-                    </template>
-
-                    <template slot="tab-pane-2">
-                    
-                    </template>
-                </tabs> -->
 
             </div>
                 
-
             <updateModal v-if="updateModal" @close="updateModalHide"/>
+
         </div>
       </div>
     </div>
     <vue-headful title="Dashboard | MANAGER"/>
- 
-
-    <div>
-    
-      <div>
-              <admin-acc/>
-      </div>
-
-       <div>
-              <admin-new-user/>
-      </div>
-    
-
-       
-    </div>
-
-    
-   
   </div>
 </template>
 
@@ -143,15 +70,19 @@ import { NavTabsCard } from "@/components";
 // modal import
 import updateModal from '../Manager/adminAccount.vue'
 
-import adminAcc from './adminAccount.vue'
-import adminNewUser from './adminAddNewUser.vue'
+//tab content import
+import admOverview from '../Manager/adminOverview.vue';
+import admManageAccount from '../Manager/adminManageAccount.vue';
+import admAuditTrail from '../Manager/adminAuditTrail.vue';
 
 export default {
   bodyClass: "profile-page",
   components: {
     updateModal, 
     NavTabsCard,
-    adminAcc,adminNewUser
+    "admin-overview": admOverview,
+    "admin-manage-account": admManageAccount,
+    "admin-audit-trail": admAuditTrail
    },
   data() {
     return {
@@ -160,6 +91,7 @@ export default {
 
        /* ADMIN INFO */
        admName: "JUAN DELA CRUZ",
+
     };
   },
   props: {
@@ -169,6 +101,7 @@ export default {
     }
   },
   computed: {
+
     headerStyle() {
       return {
         backgroundImage: `url(${this.header})`
@@ -176,12 +109,6 @@ export default {
     }
   },
 
-  methods: {
-    /*modal function*/
-    updateModalHide() {
-      this.updateModal = false;
-    },
-  }
 };
 </script>
 
@@ -193,10 +120,4 @@ export default {
 .__name h3 {
     margin-bottom: 0 !important;
 }
-
-.__tabs {
-    margin: 1.75em 1em 1.5em 1em !important;
-}
-
-
 </style>
